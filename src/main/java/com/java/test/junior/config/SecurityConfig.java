5020343a43fce359068cd30f763acc6e4df2986c
Victor Revenco <victor.revenco@isa.utm.md>
2025-05-23 14:26:03 +0300

    Finish milestone 4

--- src/main/java/com/java/test/junior/config/SecurityConfig.java
+++ src/main/java/com/java/test/junior/config/SecurityConfig.java
@@ -12,14 +12,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private static final String[] SWAGGER_WHITELIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/swagger-resources"
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/api/auth/**").permitAll()
-                        .antMatchers("/api/products/**").authenticated()
-                        .anyRequest().authenticated()
-                )
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers(SWAGGER_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+                .and()
                 .httpBasic()
                 .and()
                 .csrf().disable();
